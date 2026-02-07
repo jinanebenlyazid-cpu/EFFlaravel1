806c290d81442eb4f117bb6a44e8499101f430d4
Jinane <jinanebenliazid@gmail.com>
2026-02-07 13:54:19 +0100

    add

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request; // ✅ OBLIGATOIRE
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,8 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+       $middleware->trustProxies( '*', 
+        Request::HEADER_X_FORWARDED_FOR | 
+        Request::HEADER_X_FORWARDED_HOST | 
+        Request::HEADER_X_FORWARDED_PORT | 
+        Request::HEADER_X_FORWARDED_PROTO 
+    );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
